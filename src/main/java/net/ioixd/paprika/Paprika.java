3645e74ad6c30dfcf8ddd6fb9e17697cb1dafc33
IoIxD <alphaproject217@gmail.com>
2022-12-27 16:46:31 -0700

    merged a basic spigot plugin template

---
+++ src/main/java/net/ioixd/paprika/Paprika.java
@@ -0,0 +1,16 @@
+package net.javaforge.paprika;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Paprika extends JavaPlugin {
+
+    public void onEnable() {
+        getLogger().info("Enabled Paprika");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Disabled Paprika");
+    }
+
+}
