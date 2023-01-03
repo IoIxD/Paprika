0cda9a4e19d1c52256c79ad24467a76c7649a234
IoIxD <alphaproject217@gmail.com>
2023-01-03 00:20:24 -0700

    temporairly disable '/lua <function>'

--- src/main/java/net/ioixd/paprika/LuaFunctionCommand.java
+++ src/main/java/net/ioixd/paprika/LuaFunctionCommand.java
@@ -45,12 +45,13 @@ public class LuaFunctionCommand implements CommandExecutor {
         }
 
         // if that fails, try and search for a function based on what they provided.
-        try {
+        // TEMPORARILY DISABLED because i need to make it so that event handler functions and functions bound to minecraft commands aren't executable
+        /*try {
             this.paprika.lua.functionExecute(args[0]);
         } catch(Exception ex) {
             sender.sendMessage(ex.getMessage());
             return false;
-        }
+        }*/
         return true;
     }
 
